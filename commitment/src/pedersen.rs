d8dd446b827b112e53178460c911bdb775181a80
confuseSUN <778398626@qq.com>
2023-05-31 17:57:03 +0800

    update

--- commitment/src/pedersen.rs
+++ commitment/src/pedersen.rs
@@ -1,14 +1,20 @@
+use std::marker::PhantomData;
+
 use crate::Result;
 use ark_ec::CurveGroup;
 use ark_std::rand::Rng;
 
-pub struct Pedersen<C: CurveGroup> {
+pub struct Parameters<C: CurveGroup> {
     pub g: Vec<C::Affine>,
     pub h: C::Affine,
 }
 
+pub struct Pedersen<C: CurveGroup> {
+    _p: PhantomData<C>,
+}
+
 impl<C: CurveGroup> Pedersen<C> {
-    pub fn setup<R: Rng>(rng: &mut R, len: usize) -> Pedersen<C> {
+    pub fn setup<R: Rng>(rng: &mut R, len: usize) -> Parameters<C> {
         let mut g = Vec::with_capacity(len);
         for _ in 0..len {
             g.push(C::rand(rng).into_affine())
@@ -16,14 +22,18 @@ impl<C: CurveGroup> Pedersen<C> {
 
         let h = C::rand(rng).into_affine();
 
-        Pedersen { g, h }
+        Parameters { g, h }
     }
 
-    pub fn commit(&self, v: &[C::ScalarField], r: &C::ScalarField) -> Result<C::Affine> {
-        if v.len() > self.g.len() {
-            return Err(format!("maximum commitment is {}", self.g.len()));
+    pub fn commit(
+        params: &Parameters<C>,
+        v: &[C::ScalarField],
+        r: &C::ScalarField,
+    ) -> Result<C::Affine> {
+        if v.len() > params.g.len() {
+            return Err(format!("maximum commitment is {}", params.g.len()));
         }
 
-        Ok((C::msm_unchecked(&self.g, v) + self.h * r).into())
+        Ok((C::msm_unchecked(&params.g, v) + params.h * r).into())
     }
 }
